5365f97e8acb4d38cebe36248de2eb02fb4def77
Keegan Burke <keeganb570@gmail.com>
2026-08-29 01:15:21 -0700

    add whop pixel to root layout head for site-wide tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@500,700,800,900&display=swap"
           rel="stylesheet"
         />
+        {/* Whop tracking pixel: snippet pasted verbatim, site-wide via this
+            root layout head. Do not reformat the inner JS. */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");
+whop.setScope("biz_CJXSvqDcFQ064Z");
+whop.track("page");`,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
